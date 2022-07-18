dcf844f23d1739e09aa68fd12a84ba0192b03da5
Wellington Garcia <Wellington.Garcia@btgpactual.com>
2022-07-18 11:20:20 -0300

    Criando a gravação do registro na tabela people

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(id, name) values(1,'Wellington')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, res) => {
     res.send('<h1>full cycle</h1>')
 })
